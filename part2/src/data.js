c00c012f169ad597f3db7ed4944bd6d6d97d2a88
DokySp <uhug@naver.com>
2022-01-16 20:17:37 +0900

    강의 part 2 내용 추가

---
+++ part2/src/data.js
@@ -0,0 +1,33 @@
+export default [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+]
+
+// import / export
+//
+//  - export default
+//   -> 원하는 변수/함수/객체명 넣으면 해당 변수/함수/객체를 다른 곳에서 import해서 사용가능!
+//   -> 파일 하나당 하나만 사용가능
+//   -> 여러 개 export할 때, {var1, var2} 이런 식으로 export 가능
+//
+//  - import
+//   -> import [가져올거 이름] from [가져올 경로]
+//
